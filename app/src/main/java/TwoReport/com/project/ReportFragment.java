ee4896ee0ca1caf8b7d60aed0350191578fd5863
Winter Alava <walava@espol.edu.ec>
2020-06-27 23:39:25 -0500

    Se establecion comunicacion con la api de googlemaps, y se inserto en el ReportFragment

--- app/src/main/java/TwoReport/com/project/ReportFragment.java
+++ app/src/main/java/TwoReport/com/project/ReportFragment.java
@@ -8,21 +8,41 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class ReportFragment extends Fragment {
-
-    public ReportFragment() {
-        // Required empty public constructor
-    }
-
+public class ReportFragment extends SupportMapFragment implements OnMapReadyCallback {
+    SupportMapFragment mMapFragment;
+    private GoogleMap mMap;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_report, container, false);
+        View rootView = super.onCreateView(inflater, container, savedInstanceState);
+
+        getMapAsync(this);
+
+        return rootView;
+    }
+
+
+
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
