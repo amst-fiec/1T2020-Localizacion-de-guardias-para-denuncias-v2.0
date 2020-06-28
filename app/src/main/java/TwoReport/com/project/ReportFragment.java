e7f1336423e2b2f8551fc8330d04f14d3d305db4
Winter Alava <walava@espol.edu.ec>
2020-06-28 15:38:17 -0500

    Se altero la estructura del main para adjuntar de forma facil nuevos componentes en linearLayout

--- app/src/main/java/TwoReport/com/project/ReportFragment.java
+++ app/src/main/java/TwoReport/com/project/ReportFragment.java
@@ -7,10 +7,11 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
@@ -20,20 +21,34 @@ import com.google.android.gms.maps.model.MarkerOptions;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class ReportFragment extends SupportMapFragment implements OnMapReadyCallback {
-    SupportMapFragment mMapFragment;
+public class ReportFragment extends Fragment implements OnMapReadyCallback {
+    private SupportMapFragment mMapFragment;
     private GoogleMap mMap;
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = super.onCreateView(inflater, container, savedInstanceState);
+//        View rootView = super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment_report, container, false);
+        Button report = (Button) view.findViewById(R.id.btnReport);
 
-        getMapAsync(this);
+//        getMapAsync(this);
+        mMapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        mMapFragment.getMapAsync(this);
+//        return rootView;
 
-        return rootView;
+
+
+
+        report.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+                Toast.makeText(getActivity(),"REPORTADO",Toast.LENGTH_LONG).show();
+            }
+        });
+        return view;
     }
-
 
 
 
@@ -45,4 +60,7 @@ public class ReportFragment extends SupportMapFragment implements OnMapReadyCall
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
+
+
+
 }
