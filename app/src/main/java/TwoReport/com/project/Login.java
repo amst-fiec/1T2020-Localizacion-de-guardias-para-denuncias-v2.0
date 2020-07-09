60f41cc296ca41173ac358c407018507db0e6b3b
Adriana Murrieta <avmurrie@espol.edu.ec>
2020-07-09 02:40:28 -0500

    Cambios Login

--- app/src/main/java/TwoReport/com/project/Login.java
+++ app/src/main/java/TwoReport/com/project/Login.java
@@ -2,7 +2,9 @@ package TwoReport.com.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Login extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void toMainView(View view){
+        Intent i = new Intent(Login.this,MainActivity.class);
+        startActivity(i);
     }
 }
