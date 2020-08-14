6327dff00c15840ff874b2f67231ca918fc0b794
Adriana Murrieta <avmurrie@espol.edu.ec>
2020-08-14 06:09:15 -0500

    Reportar Crimen

--- app/src/main/java/TwoReport/com/project/ReportCrime.java
+++ app/src/main/java/TwoReport/com/project/ReportCrime.java
@@ -5,13 +5,39 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class ReportCrime extends AppCompatActivity {
-
+    private TextView t;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report_crime);
+        Spinner spinner = (Spinner) findViewById(R.id.spinnerTipoDelito);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.tipoCrimen_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
     }
+
+    public void reportar(View view){
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("Denuncia");
+
+        Toast.makeText(getApplicationContext(),"REPORTADO", Toast.LENGTH_LONG).show();
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+
+    }
+
+
 
 }
